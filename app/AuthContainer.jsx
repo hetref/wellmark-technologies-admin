22178b3ecb9b206b0c0953515a4a43c8e74eba3b
Prg-Yash <127442680+Prg-Yash@users.noreply.github.com>
2024-08-18 18:08:30 +0530

    few changessss

--- app/AuthContainer.jsx
+++ app/AuthContainer.jsx
@@ -2,24 +2,33 @@
 import { auth } from "@/firebase/config";
 import { onAuthStateChanged } from "firebase/auth";
 import { useRouter } from "next/navigation";
-import React, { useEffect } from "react";
-import toast from "react-hot-toast";
+import React, { useEffect, useState } from "react";
 
 const AuthContainer = ({ children }) => {
+  const [loading, setLoading] = useState(true);
   const router = useRouter();
+
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         router.push("/");
       } else {
-        // toast.error("User ");
         router.push("/sign-in");
       }
+      setLoading(false); // Set loading to false after the user state is checked
     });
 
-    // Cleanup subscription on unmount
     return () => unsubscribe();
   }, [router]);
+
+  if (loading) {
+    return (
+      <div className="w-screen h-screen flex justify-center items-center">
+        <h1 className="text-2xl font-semibold animate-pulse">Loading...</h1>
+      </div>
+    );
+  }
+
   return <div>{children}</div>;
 };
 
